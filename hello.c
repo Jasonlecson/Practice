f55dece59a24ec19eed1c507ae4247fa8594e820
Jasonlecson <lujinmiao@outlook.com>
2016-05-04 22:07:56 +0800

    新文件：   hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void ap(void);
+int main(void)
+{
+int dog;
+    ap();
+	printf("How many dogs do you have?\n");
+	printf("----------\b\b\b\b\b\b\b\b\b\b");
+	scanf("%d",&dog);
+	printf("So you have %d dogs!\n",dog);
+return 0;
+}
+void ap(void)
+ {
+ printf("Hello!\n");
+ }
