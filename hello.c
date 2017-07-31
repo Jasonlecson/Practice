bb7000e2fac903bd596348ca1754ac5a500c04bb
Jasonlecson <lujinmiao@outlook.com>
2017-07-31 06:43:12 +0000

    new file:   hello 	modified:   hello.c

--- hello.c
+++ hello.c
@@ -7,7 +7,8 @@ int dog;
 	printf("How many dogs do you have?\n");
 	printf("----------\b\b\b\b\b\b\b\b\b\b");
 	scanf("%d",&dog);
-	printf("So you have %d dogs!\n",dog);
+	printf("So,the price is $200 per dog\n");
+	printf("So you have %d dogs! It means you have %d dollars!\n",dog,dog*200);
 return 0;
 }
 void ap(void)
